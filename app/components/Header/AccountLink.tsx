1f41de9c71f225a86d7700847e91f0e2433c6b88
Michail Kalogeras <89188677+MK-XXXVII@users.noreply.github.com>
2024-01-04 22:36:32 +0100

    yarn installation

--- app/components/Header/AccountLink.tsx
+++ app/components/Header/AccountLink.tsx
@@ -1,9 +1,13 @@
 import {useMatches, Link} from '@remix-run/react';
 import {IconAccount, IconLogin} from '../Icon';
 
+interface MatchData {
+  isLoggedIn: boolean;
+}
+
 export function AccountLink({className}: {className?: string}) {
   const [root] = useMatches();
-  const isLoggedIn = root.data?.isLoggedIn;
+  const isLoggedIn = (root.data as MatchData).isLoggedIn;
   return isLoggedIn ? (
     <Link to="/account" className={className}>
       <IconAccount />
@@ -14,3 +18,4 @@ export function AccountLink({className}: {className?: string}) {
     </Link>
   );
 }
+
